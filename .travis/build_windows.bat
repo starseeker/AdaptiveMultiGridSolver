62e84e7669df0e162b51f78235308a9419736dc7
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-09-28 16:38:29 -0400

    not using travis

--- .travis/build_windows.bat
+++
@@ -1,5 +0,0 @@
-
-conan install . -if build --update --build missing -o amgs:build_apps=True -pr %CONAN_PROFILE%
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DBUILD_APPS=ON -DFAST_COMPILE=ON -G "Visual Studio 16 2019" -A x64 ..
-travis-pls cmake --build . --config Release -j %BUILD_THREADS%
--- .travis/test_windows_packaging.bat
+++
@@ -1,2 +0,0 @@
-
-conan create . user/channel -pr %CONAN_PROFILE%
